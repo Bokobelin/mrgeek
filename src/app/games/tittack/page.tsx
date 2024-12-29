bcb8a0b97c042db69b05a82a72b47f45c96bfd71
Bokobelin <matthieu.richelle@gmail.com>
2024-12-29 10:24:32 +0100

    Fix page link

--- src/app/games/tittack/page.tsx
+++ src/app/games/tittack/page.tsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
-
 export default function Tittack() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-gray-100">
         <main className="flex flex-col gap-8 row-start-2 items-center">
           <div className="flex flex-col gap-4 items-center">
             <h1 className="text-5xl font-bold">Ti&apos;ttack</h1>
-            <Link href="/games/ti'ttack/downloads/tittackpre-alpha0.0.2.zip" className="text-2xl underline">Télécharger le jeu</Link>
+            <a href="/games/ti'ttack/downloads/tittackpre-alpha0.0.2.zip" download className="text-2xl underline">Télécharger le jeu</a>
           </div>
         </main>
       </div>
