a825a27d416ecc77acb248c476d0bcf910371fe1
Bokobelin <matthieu.richelle@gmail.com>
2024-12-29 10:15:20 +0100

    Add game

---
+++ src/app/games/ti'ttack/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Tittack() {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-gray-100">
+        <main className="flex flex-col gap-8 row-start-2 items-center">
+          <div className="flex flex-col gap-4 items-center">
+            <h1 className="text-5xl font-bold">Ti&apos;ttack</h1>
+            <Link href="/games/ti'ttack/downloads/ti'ttack pre-alpha 0.0.2.zip" className="text-2xl underline">Télécharger le jeu</Link>
+          </div>
+        </main>
+      </div>
+    );
+}
